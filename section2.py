9411f54a45fa857f927e2f3c168e63847586434f
Pierre <pierrohockers@hotmail.com>
2020-03-03 18:46:55 +0100

    added a better output to section 2

--- section2.py
+++ section2.py
@@ -8,5 +8,9 @@ if __name__ == '__main__':
     game = Game(0,0, policy, steps)
     game.playGame()
 
+    print(" Section 2 : \n")
+    print(" Printing the trajectory of the game (" + str(steps) + " steps) under the following format : \n")
+    print("(position, speed), action, reward")
+    
     for tuple in game.trajectory:
         print(tuple)
